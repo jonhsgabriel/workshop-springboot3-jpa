f30190760db130d5e115660a8f58b75b66de01af
Joao Gabriel <jgbrlmotta@gmail.com>
2023-07-17 09:43:26 -0300

    Payment, one-to-one association

--- src/main/java/com/joaogabriel/course/config/TestConfig.java
+++ src/main/java/com/joaogabriel/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.joaogabriel.course.entities.Category;
 import com.joaogabriel.course.entities.Order;
 import com.joaogabriel.course.entities.OrderItem;
+import com.joaogabriel.course.entities.Payment;
 import com.joaogabriel.course.entities.Product;
 import com.joaogabriel.course.entities.User;
 import com.joaogabriel.course.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
